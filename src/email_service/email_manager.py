7194fa930055a5e50b5616453c2f1d56fefb6246
Oliver Cronk <ojc011@yahoo.com>
2023-09-06 12:25:23 -0400

    Email successfully sends through gui

--- src/email_service/email_manager.py
+++ src/email_service/email_manager.py
@@ -1,6 +1,6 @@
 import smtplib
-import os
 from email.mime.text import MIMEText
+import hashlib
 
 
 def send_email(receiver_email, subject, body, smtp_config):
@@ -11,9 +11,10 @@ def send_email(receiver_email, subject, body, smtp_config):
     msg["Subject"] = subject
 
     try:
-        with smtplib.SMTP_SSL(
+        with smtplib.SMTP(
             smtp_config["smtp_server"], smtp_config["smtp_port"]
         ) as server:
+            server.starttls()
             server.login(smtp_config["sender_email"], smtp_config["password"])
             server.sendmail(
                 smtp_config["sender_email"], receiver_email, msg.as_string()
@@ -24,36 +25,7 @@ def send_email(receiver_email, subject, body, smtp_config):
         return False
 
 
-import hashlib
-
-
 def generate_unsubscribe_link(base_url, email):
     """Generate a unique unsubscribe link for the given email."""
-    # Create a simple hash for the email
     email_hash = hashlib.md5(email.encode()).hexdigest()
     return f"{base_url}/unsubscribe/{email_hash}"
-
-
-OUTLOOK_EMAIL = os.environ.get("OUTLOOK_EMAIL")
-OUTLOOK_PASSWORD = os.environ.get("OUTLOOK_PASSWORD")
-
-if __name__ == "__main__":
-    SMTP_CONFIG = {
-        "sender_email": OUTLOOK_EMAIL,
-        "password": OUTLOOK_PASSWORD,
-        "smtp_server": "smtp-mail.outlook.com",
-        "smtp_port": 587,
-    }
-
-    receiver = "receiver@example.com"
-    subject = "Test Email with Unsubscribe"
-
-    # Generate the unsubscribe link
-    unsubscribe_link = generate_unsubscribe_link("http://yourwebsite.com", receiver)
-    body = f"""
-        <p>Hello there!</p>
-        <p>This is a test email.</p>
-        <p>If you'd like to unsubscribe, <a href="{unsubscribe_link}">click here</a>.</p>
-    """
-
-    send_email(receiver, subject, body, SMTP_CONFIG)

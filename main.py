34dbd70f3f32b97e3ddb24d4da8ea1bd157d8ad7
Oliver Cronk <ojc011@yahoo.com>
2023-09-07 12:35:13 -0400

    Added better logging, presets button, conditional emailing based on values, etc

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import time
 import random
 import threading
 import shutil
+import json
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
@@ -24,29 +25,43 @@ def send_email(receiver_email, subject, body):
     msg["Subject"] = subject
 
     email_template = f"""
-    <div style="border: 2px solid black; padding: 20px; width: 500px; margin: 20px auto;">
-        <div style="display: flex; align-items: center;">
-            <img src="cid:logo" alt="Logo" style="max-width: 100px; max-height: 100px; margin-right: 20px;" />
-            <h2 style="flex-grow: 0; text-align: center; margin: 0 auto;">Hayden Building Maintenance Corporation</h2>
+    <div style="border: 2px solid black; padding: 20px; width: 600px; margin: 20px auto;">
+        <div style="text-align: center; padding-bottom: 10px;">
+            <img src="cid:logo" alt="Logo" style="max-width: 100px; max-height: 100px; margin-bottom: 10px;" />
+            <h2 style="margin: 0;">Hayden Building Maintenance Corporation</h2>
         </div>
         <hr>
-        <div style="text-align: center; font-size: 18px; line-height: 2.0;">
+        <div style="text-align: center; font-size: 18px; line-height: 2.0; padding: 10px 0;">
             {body}
         </div>
         <hr>
-        <div style="border: 1px solid black; padding: 10px; text-align: center; margin-top: 20px;">
-            <a href="https://roofline.com" style="color: blue; text-decoration: none; margin-bottom: 10px; display: block;">Visit Our Website</a>
+        <div style="text-align: center;">
+            <p>Oliver J. Cronk</p>
+            <p>Marketing Coordinator</p>
+            <p>Hayden Building Maintenance Corp.</p>
+            <p>ROOFING * WATERPROOFING * RESTORATION</p>
+            <p>169 Western Highway - PO Box G</p>
+            <p>West Nyack, NY 10994</p>
+            <p>Office: 845-353-3400 ext. 136</p>
+            <p>Cell: 845-925-2357</p>
+        </div>
+        <hr>
+        <div style="padding: 5px; text-align: center; margin-top: 10px;">
+            <a href="https://roofline.com" style="color: blue; text-decoration: none; margin-bottom: 5px; display: block;">Visit Our Website</a>
             <a href="mailto:olivercronk@roofline.com?subject=Unsubscribe&body=Please unsubscribe me from this mailing list." style="color: red; text-decoration: none;">Unsubscribe</a>
         </div>
     </div>
     """
     msg.attach(MIMEText(email_template, "html"))
 
-
-    with open("assets/hbm.png", "rb") as img:
-        mime_img = MIMEImage(img.read())
-        mime_img.add_header("Content-ID", "<logo>")
-        msg.attach(mime_img)
+    try:
+        with open("assets/hbm.png", "rb") as img:
+            mime_img = MIMEImage(img.read())
+            mime_img.add_header("Content-ID", "<logo>")
+            msg.attach(mime_img)
+    except Exception as e:
+        app.update_log(f"Error loading image: {e}")
+        return False
 
     try:
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
@@ -55,7 +70,7 @@ def send_email(receiver_email, subject, body):
             server.sendmail(GMAIL_EMAIL, receiver_email, msg.as_string())
         return True
     except Exception as e:
-        print(f"Error sending email: {e}")
+        app.update_log(f"Error sending email: {e}")
         return False
 
 
@@ -63,24 +78,40 @@ class EmailAutomationApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Email Automation")
-        self.root.geometry("1920x1080")
+        self.root.geometry("720x1080")
+        self.current_index = 0
+        self.total_count = 0
+        self.sent_count = 0
 
         self.csv_filepath = None
 
         self.upload_btn = tk.Button(
             self.root, text="Upload CSV", command=self.upload_csv_callback
         )
-        self.upload_btn.pack(pady=20)
+        self.upload_btn.pack(pady=10)
 
-        tk.Label(self.root, text="Subject:").pack(pady=10)
+        self.csv_label = tk.Label(self.root, text="")
+        self.csv_label.pack(pady=2.5)
+
+        tk.Label(self.root, text="Subject:").pack(pady=5)
         self.subject_entry = tk.Entry(self.root, width=50)
         self.subject_entry.pack()
 
-        tk.Label(self.root, text="Email Body:").pack(pady=10)
+        tk.Label(self.root, text="Email Body:").pack(pady=5)
         self.message_entry = tk.Text(self.root, height=10, width=50)
         self.message_entry.pack()
 
-        tk.Label(self.root, text="Status Log:").pack(pady=10)
+        self.save_preset_btn = ttk.Button(
+            self.root, text="Save Preset", command=self.save_preset, style="BW.TButton"
+        )
+        self.save_preset_btn.pack(pady=10)
+
+        self.load_preset_btn = ttk.Button(
+            self.root, text="Load Preset", command=self.load_preset, style="BW.TButton"
+        )
+        self.load_preset_btn.pack(pady=10)
+
+        tk.Label(self.root, text="Status Log:").pack(pady=5)
         self.log_textbox = tk.Text(
             self.root,
             height=10,
@@ -89,7 +120,7 @@ class EmailAutomationApp:
             fg="black",
             state=tk.DISABLED,
         )
-        self.log_textbox.pack(pady=10)
+        self.log_textbox.pack(pady=5)
 
         style = ttk.Style()
         style.configure("BW.TButton", foreground="white", background="black")
@@ -101,7 +132,7 @@ class EmailAutomationApp:
             state=tk.DISABLED,
             style="BW.TButton",
         )
-        self.send_btn.pack(pady=20)
+        self.send_btn.pack(pady=10)
 
         self.stop_flag = threading.Event()
 
@@ -112,7 +143,7 @@ class EmailAutomationApp:
             state=tk.DISABLED,
             style="BW.TButton",
         )
-        self.stop_btn.pack(pady=20)
+        self.stop_btn.pack(pady=10)
 
         self.download_btn = ttk.Button(
             self.root,
@@ -121,19 +152,43 @@ class EmailAutomationApp:
             state=tk.DISABLED,
             style="BW.TButton",
         )
-        self.download_btn.pack(pady=20)
+        self.download_btn.pack(pady=10)
+
+    def save_preset(self):
+        preset_data = {
+            "subject": self.subject_entry.get(),
+            "body": self.message_entry.get("1.0", tk.END),
+        }
+        try:
+            with open("presets.json", "a") as file:
+                json.dump(preset_data, file)
+                file.write("\n")
+        except Exception as e:
+            self.update_log(f"Error saving preset: {e}")
+
+    def load_preset(self):
+        try:
+            with open("presets.json", "r") as file:
+                presets = [json.loads(line) for line in file]
+        except FileNotFoundError:
+            presets = []
+
+        if presets:
+            # Implement a GUI for the user to choose a preset
+            chosen_preset = random.choice(presets)  # Replace with a GUI to choose
+            self.subject_entry.delete(0, tk.END)
+            self.subject_entry.insert(0, chosen_preset["subject"])
+            self.message_entry.delete("1.0", tk.END)
+            self.message_entry.insert("1.0", chosen_preset["body"])
+        else:
+            messagebox.showinfo("Info", "No presets found.")
 
     def upload_csv_callback(self):
         filepath = filedialog.askopenfilename()
-        if not filepath:
-            return
-
-        if not filepath.endswith(".csv"):
-            messagebox.showerror("Error", "Please select a valid CSV file!")
-            return
-
-        self.csv_filepath = filepath
-        self.send_btn["state"] = tk.NORMAL
+        if filepath:
+            self.csv_filepath = filepath
+            self.csv_label.config(text=os.path.basename(filepath))
+            self.send_btn["state"] = tk.NORMAL
 
     def update_log(self, message):
         self.log_textbox.config(state=tk.NORMAL)
@@ -153,26 +208,49 @@ class EmailAutomationApp:
             reader = csv.reader(csv_file)
             rows = list(reader)
 
-        sent_count = 0
-        for index, row in enumerate(rows[1:], start=1):
-            if not row[14]:
+        # Determine the valid email count
+        valid_email_count = sum(
+            1
+            for row in rows[1:]
+            if row[0] and row[1].lower() != "skip" and row[2] != "1"
+        )
+
+        self.total_count = valid_email_count
+        self.sent_count = 0
+
+        attempt_count = 0  # Initialize a counter to keep track of email attempts
+
+        for index, row in enumerate(
+            rows[self.current_index + 1 :], start=self.current_index + 1
+        ):
+            if not row[0]:
                 self.update_log("Encountered an empty email. Process stopped.")
                 break
+
+            if row[1].lower() == "skip" or row[2] == "1":
+                if row[1].lower() == "skip":
+                    self.update_log(f"Skipping {row[0]}: Unsubscribed")
+                if row[2] == "1":
+                    self.update_log(f"Skipping {row[0]}: Already Sent")
+                continue
+
+            attempt_count += 1  # Increment the attempt count
 
             if self.stop_flag.is_set():
                 self.update_log("Process stopped by user.")
                 break
 
-            if send_email(row[14], subject, message):
-                sent_count += 1
+            if send_email(row[0], subject, message):
+                self.sent_count += 1
                 self.update_log(
-                    f"Successfully sent to: {row[14]} - {sent_count}/{len(rows[1:])}"
+                    f"Successfully sent to: {row[0]} - {self.sent_count}/{attempt_count}"
                 )
-                row[1] = "1"
+                row[2] = "1"  # Mark as sent
             else:
-                self.update_log(f"Failed to send to: {row[14]}")
+                self.update_log(f"Failed to send to: {row[0]}")
 
-            time.sleep(random.uniform(15, 20))
+            self.current_index = index
+            time.sleep(random.uniform(2, 5))
 
         with open(
             "updated_" + os.path.basename(self.csv_filepath),
@@ -183,9 +261,12 @@ class EmailAutomationApp:
             writer = csv.writer(csv_file)
             writer.writerows(rows)
 
-        self.update_log(f"Completed: Sent {sent_count}/{len(rows[1:])} emails!")
+        self.update_log(f"Completed: Sent {self.sent_count}/{attempt_count} emails!")
         messagebox.showinfo("Completed", "Emails sent!")
         self.download_btn["state"] = tk.NORMAL
+        self.send_btn["state"] = tk.NORMAL
+        self.send_btn["text"] = "Send Emails"
+        self.stop_btn["state"] = tk.DISABLED
 
     def stop_process(self):
         self.stop_flag.set()
@@ -193,17 +274,15 @@ class EmailAutomationApp:
     def download_csv(self):
         filepath = "updated_" + os.path.basename(self.csv_filepath)
         if filepath:
-            shutil.copy(
-                filepath,
-                filedialog.asksaveasfilename(
-                    defaultextension=".csv", filetypes=[("CSV files", "*.csv")]
-                ),
-            )
+            shutil.copy(self.csv_filepath, filepath)
+            self.update_log(f"CSV downloaded as {filepath}")
 
     def send_emails_callback(self):
-        self.stop_flag.clear()
         self.send_btn["state"] = tk.DISABLED
         self.stop_btn["state"] = tk.NORMAL
+
+        self.stop_flag.clear()
+
         email_thread = threading.Thread(target=self.send_emails)
         email_thread.start()
 
